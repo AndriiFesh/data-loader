a3f93946a32b0d0b899d2bb50c238d1104120091
krabic <grigoryvoloshyn@gmail.com>
2023-09-29 20:10:51 +0300

    fix acts and bash file

--- acts.py
+++ acts.py
@@ -1,33 +1,32 @@
-# import requests as rq
-import glob
-
-import aiohttp
 import asyncio
-from lxml import html
-import undetected_chromedriver as uc
+import glob
 import json
 import os
+
+import aiohttp
+import undetected_chromedriver as uc
+from lxml import html
 from tqdm import tqdm
-import hashlib
 
 data = {
-    'Acts': 'https://sso.agc.gov.sg/Browse/Act/Current/All?SortBy=Title&SortOrder=ASC',
-    'Subsidiary Legislation': 'https://sso.agc.gov.sg/Browse/SL/Current/All?PageSize=20',
-    'Acts Supplement': 'https://sso.agc.gov.sg/Browse/Acts-Supp/Published/All?PageSize=20',
-    'Bills Supplement': 'https://sso.agc.gov.sg/Browse/Bills-Supp/Published/All?PageSize=20',
-    'Subsidiary Legislation Supplement': 'https://sso.agc.gov.sg/Browse/SL-Supp/Published/All?PageSize=20',
-    'Revised Editions of Acts': 'https://sso.agc.gov.sg/Browse/Act-Rev/Published/All?PageSize=20',
-    'Revised Editions of Subsidiary Legislation': 'https://sso.agc.gov.sg/Browse/SL-Rev/Published/All?PageSize=20',
+    "Acts": "https://sso.agc.gov.sg/Browse/Act/Current/All?SortBy=Title&SortOrder=ASC",
+    "Subsidiary Legislation": "https://sso.agc.gov.sg/Browse/SL/Current/All?PageSize=20",
+    "Acts Supplement": "https://sso.agc.gov.sg/Browse/Acts-Supp/Published/All?PageSize=20",
+    "Bills Supplement": "https://sso.agc.gov.sg/Browse/Bills-Supp/Published/All?PageSize=20",
+    "Subsidiary Legislation Supplement": "https://sso.agc.gov.sg/Browse/SL-Supp/Published/All?PageSize=20",
+    "Revised Editions of Acts": "https://sso.agc.gov.sg/Browse/Act-Rev/Published/All?PageSize=20",
+    "Revised Editions of Subsidiary Legislation": "https://sso.agc.gov.sg/Browse/SL-Rev/Published/All?PageSize=20",
 }
 
 
 async def create_driver() -> uc.Chrome:
-    '''Function to create webdriver for rendering webpages
+    """Function to create webdriver for rendering webpages
     Returns:
     driver (uc.Chrome): Webdriver to render the page before scrapping
-    '''
+    """
+    uc.find_chrome_executable()
     options = uc.ChromeOptions()
-    options.add_argument('--incognito')
+    options.add_argument("--incognito")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--start-maximized")
 
@@ -38,20 +37,21 @@ async def create_driver() -> uc.Chrome:
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-setuid-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    # options.add_argument("--headless=new") #can enable or disable headless, up to your needs
-
-    return uc.Chrome(options=options, version_main=115)
+    options.add_argument("--headless=new")
+    options.binary_location = "/usr/bin/chromium-browser"
+    print("Chrome Binary Location:", options.binary_location)
+    return uc.Chrome(options=options, version_main=117)
 
 
 async def get_info_from_page(item: dict, driver: uc.Chrome, session: aiohttp.ClientSession):
-    '''Using the webdriver get to the page, collect the data
+    """Using the webdriver get to the page, collect the data
     Parameters:
     item (dict): Dictionary containing the title and the url
     driver (uc.Chrome): Webdriver to render the page before scrapping
     Returns:
     item (dict): Modified dictionary with the data from page
     driver (uc.Chrome): Webdriver to render the page before scrapping
-    '''
+    """
     driver.get(f'{item["url"]}?WholeDoc=1')
     await asyncio.sleep(30)
     tree = html.fromstring(driver.page_source)
@@ -60,45 +60,43 @@ async def get_info_from_page(item: dict, driver: uc.Chrome, session: aiohttp.Cli
     if main:
         front = main[0].xpath('./div/div[@class="front"]')
         if front:
-            front_string = front[0].xpath('.//text()')
+            front_string = front[0].xpath(".//text()")
         else:
-            front_string = ''
-        item['front'] = front_string
+            front_string = ""
+        item["front"] = front_string
         body = main[0].xpath('./div/div[@class="body"]')
         if body:
-            tables_in_body = body[0].xpath('./div')
+            tables_in_body = body[0].xpath("./div")
             body_data = []
             for i in range(len(tables_in_body)):
                 item_table = {}
-                tbls = tables_in_body[i].xpath('./table')  # gather text
+                tbls = tables_in_body[i].xpath("./table")  # gather text
                 if len(tbls) == 2:
                     try:
-                        key = tbls[0].xpath('./tbody/tr/td//text()')[0]
-                        value = tbls[1].xpath('./tbody/tr/td//text()')
+                        key = tbls[0].xpath("./tbody/tr/td//text()")[0]
+                        value = tbls[1].xpath("./tbody/tr/td//text()")
                         item_table[key] = value
                     except IndexError:
-                        print('Empty key, saving in raw as well')
+                        print("Empty key, saving in raw as well")
                 else:
                     try:
-                        value = tbls[0].xpath('./tbody/tr/td//text()')
+                        value = tbls[0].xpath("./tbody/tr/td//text()")
                         body_data[i - 1][key] = value
                     except IndexError:
-                        print('Table is broken, saving as raw as well', item["url"])
+                        print("Table is broken, saving as raw as well", item["url"])
                     except UnboundLocalError:
-                        print(
-                            'No table in document, will be saved as raw as well')
+                        print("No table in document, will be saved as raw as well")
                 body_data.append(item_table)
-                item['body'] = body_data
-            tables_in_body = main[0].xpath(
-                './div/div[@class="body"]//text()')
-            item['body_raw'] = tables_in_body
+                item["body"] = body_data
+            tables_in_body = main[0].xpath('./div/div[@class="body"]//text()')
+            item["body_raw"] = tables_in_body
         else:
-            print(f'No body found {driver.current_url}')
+            print(f"No body found {driver.current_url}")
         tail = main[0].xpath('./div/div[@class="tail"]//text()')
         if tail:
-            item['tail_data'] = tail
+            item["tail_data"] = tail
     else:
-        print(f'No main body {driver.current_url}')
+        print(f"No main body {driver.current_url}")
 
     elms = tree.xpath('//div[@class="prov1"]')
 
@@ -113,30 +111,26 @@ async def get_info_from_page(item: dict, driver: uc.Chrome, session: aiohttp.Cli
 
 async def get_number_of_pages(driver: uc.Chrome, session: aiohttp.ClientSession) -> int:
     tree = html.fromstring(driver.page_source)
-    number = tree.xpath(
-        '//div[@class="col-sm-12 no-side-padding page-count"]/text()')
+    number = tree.xpath('//div[@class="col-sm-12 no-side-padding page-count"]/text()')
 
     if number:
-        return int(number[0].strip().split(' ')[3])
+        return int(number[0].strip().split(" ")[3])
     else:
-        print(
-            f'No number of pages was found for {driver.current_url}\nReturning 1')
+        print(f"No number of pages was found for {driver.current_url}\nReturning 1")
         return 1
 
 
 async def get_urls_from_single_page(driver: uc.Chrome, session: aiohttp.ClientSession) -> list[dict]:
     tree = html.fromstring(driver.page_source)
-    urls = tree.xpath(
-        '//table[@class="table browse-list"]/tbody/tr/td[not(@class)]/a[@class="non-ajax"]/@href')
-    tags = tree.xpath(
-        '//table[@class="table browse-list"]/tbody/tr/td[not(@class)]/a[@class="non-ajax"]/text()')
+    urls = tree.xpath('//table[@class="table browse-list"]/tbody/tr/td[not(@class)]/a[@class="non-ajax"]/@href')
+    tags = tree.xpath('//table[@class="table browse-list"]/tbody/tr/td[not(@class)]/a[@class="non-ajax"]/text()')
     inter_data = []
     for i in range(len(urls)):
         item = {}
-        item['url'] = f'https://sso.agc.gov.sg{urls[i]}'
-        item['title'] = tags[i]
+        item["url"] = f"https://sso.agc.gov.sg{urls[i]}"
+        item["title"] = tags[i]
         inter_data.append(item)
-    print(f'Page: {driver.current_url}, urls scraped: {len(inter_data)}')
+    print(f"Page: {driver.current_url}, urls scraped: {len(inter_data)}")
     return inter_data
 
 
@@ -162,7 +156,7 @@ async def scrape_and_save_data(key, val):
     async with aiohttp.ClientSession() as session:
         driver = await create_driver()
 
-        print(f'Now scrapping {key}\nStarting url:{val}')
+        print(f"Now scrapping {key}\nStarting url:{val}")
 
         directory_name = "data/" + key
 
@@ -173,7 +167,7 @@ async def scrape_and_save_data(key, val):
 
         final = []
         for i in range(await get_number_of_pages(driver=driver, session=session)):
-            driver.get(val.replace('/All', f'/All/{i}?'))
+            driver.get(val.replace("/All", f"/All/{i}?"))
             await asyncio.sleep(10)
             final.extend(await get_urls_from_single_page(driver=driver, session=session))
 
@@ -188,10 +182,10 @@ async def scrape_and_save_data(key, val):
             with open(f"{directory_name}/{url}.json", "w") as f:
                 json.dump(final[j], f)
 
-        with open(f'data/{key}.json', 'w') as f:
+        with open(f"data/{key}.json", "w") as f:
             json.dump(final, f)
 
-        print(f'Data for {key} was collected, saved in {directory_name}.json\n')
+        print(f"Data for {key} was collected, saved in {directory_name}.json\n")
 
         combined(directory_name, key)
 
@@ -205,6 +199,6 @@ async def main():
     await asyncio.gather(*tasks)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
 # https://sso.agc.gov.sg/Act/IRDA2018?WholeDoc=1#xy-
--- run_scraper.sh
+++ run_scraper.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-pip install lxml undetected-chromedriver requests tqdm aiohttp
-# Create and activate a virtual environment
+# Create and activate a Python virtual environment
 python3 -m venv venv
 source venv/bin/activate
 
 # Install required Python packages
 pip install -r requirements.txt
 
+# Install Chromium (Chromium is an open-source version of Chrome)
+sudo apt-get update
+sudo apt-get install -y chromium-browser
+
 # Run the Python acts.py script
 python3 acts.py
 
 python3 indexing_v2.py
 
-git checkout -b feature/add-new-embeddings
+git checkout -b data-loader
 
 dvc pull
 
-cp ft_embeddings/index.faiss  #TODO add path to repo
-cp ft_embeddings/index.pkl  #TODO add path to repo
+cp ft_embeddings/index.faiss faiss_index_acts.dvc
 
-git add index.faiss index.pkl
+git add ft_embeddings/index.faiss ft_embeddings/index.pkl
 
-git commit -m "add new index files index.faiss и index.pkl"
+git commit -m "add new index files index.faiss и index.pkl" -n
 
-git push
+git push & wait
 
 deactivate
-
